6829cdacf1d6ab7faaa584a71a2361e18e8af999
Malcev Nikita <maltsew.na@gmail.com>
2022-04-26 22:42:05 +0300

    WSGI приложение и дополненный конфиг для запуска gunicorn

---
+++ hello.py
@@ -0,0 +1,14 @@
+def wsgi_application(environ, start_response):
+
+    # основная логика WSGI приложения
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+        ]
+    #Дефолтный файл приложения подразумевает, что входной параметр будет один
+    #а в тесте указано, что на вход подается группа параметров
+    #в таком случае - генератор списка параметров
+    #UPD: параметры передаеются в байтовом виде
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, header)
+    return body
