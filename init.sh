79d70d7ba6c89793322860ed9984defcf5084755
Malcev Nikita <maltsew.na@gmail.com>
2022-05-08 22:21:56 +0300

    настройки и конфиг файл для gunicorn

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
