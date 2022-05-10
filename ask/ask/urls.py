a30effe54823297e304d616edaaba847c518ac70
Malcev Nikita <maltsew.na@gmail.com>
2022-05-10 21:44:25 +0300

    fix urls.py for empty path. Add new shell file for run gunicron as deamon

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from qa import views
 
 urlpatterns = [
-    path('', views.test, name='/'),
+    path('^$', views.test, name='/'),
     path('login/', views.test, name='login'),
     path('signup/', views.test, name='signup'),
     path('question/<int:id>/', views.test, name='question'),
